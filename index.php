6a25dcdda2698d1b278b9597ce99cdd956dd7632
tobeworks <hallo@tobeworks.de>
2021-04-22 14:03:06 +0200

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       Om Group Style
+ * Plugin URI:        
+ * Description:       Loginstyle
+ * Version:           1.0
+ * Author:            Tobias Lorsbach
+ * License:           GPL v2 or later
+ * Text Domain:       om-styles
+ * Domain Path:       /languages
+ */
+$loginfile = plugin_dir_path(__FILE__).'/login/login.php';
+require_once($loginfile);
---
+++ login/login.php
@@ -0,0 +1,42 @@
+<?php
+	
+/**
+ * Admin Logo Replacement
+*/
+function omLoginLogo() { 
+	//Style auch bei Login einfügen
+	wp_enqueue_style( 'wpbf-style-child', WPBF_CHILD_THEME_URI . '/login/login.css', false, WPBF_CHILD_VERSION );
+?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+        background-image: url(<?= plugin_dir_url(__FILE__).'../images/omgrouplogo.svg'; ?>);
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'omLoginLogo');
+
+/**
+ * Admin Klick URL
+*/
+function omLoginUrl() {
+    return 'https://online-marketing-group.ch/';
+}
+add_filter( 'login_headerurl', 'omLoginUrl' );
+
+/**
+ * Admin Logo Text
+*/
+function omLoginText() {
+    return 'The site brought you by Online-Marketing-Group';
+}
+add_filter( 'login_headertitle', 'omLoginText');
+
+
+/**
+ * Additional texts
+*/
+function omLoginFormTop(){
+	return '<div class="om-loginform-top">Support: <a href="mailto:support@omgroup.ch?subject=Supportanfrage über '.home_url().'">support@omgroup.ch</a><br>
+Tel: +41 44 536 01 37</div>';
+}
+add_filter( 'login_message', 'omLoginFormTop');
